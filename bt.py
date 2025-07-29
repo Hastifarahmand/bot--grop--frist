565c4dd40cf4a6b3b4bea35d7f322415d720c9f3
maryam Rezanezhad Farahmand <hastifrm81@gmail.com>
2025-07-29 22:03:39 +0330

    4 commited

--- bt.py
+++ bt.py
@@ -6,21 +6,26 @@ bot=telebot.TeleBot("8045809996:AAGfi2BQw-jmhHBZPEY5PLcU72ZZIBQC8TA")
 @bot.message_handler(content_types=['new_chat_members'])
 def welcome_to_new_members(message):
     for new_member in message.new_chat_members:
-        bot.send_message(message.chat.id,f"welcome to grop{message}")
+        bot.send_message(message.chat.id,f"خوش اومدی به گروه {message.from_user.first_name}")
 
 
 @bot.message_handler(regexp="خر")
 def regexp_message(message):
-    bot.reply_to(message, "What your saying is not appropriate here.")
+    bot.reply_to(message, "حرفی که زدی درست نبود")
+
+
 bot.infinity_polling()
 
-@bot.message_handler(commands="/hello ")
-def wel_hel_han(message):
-    bot.reply_to(message ,"hi {message.chat.id}")
-@bot.message_handler(content_types=['new_ch'
-'at_members'])
-@bot.message_handler("chat_id"=CHAT_ID)
+# @bot.message_handler(commands="/hello ")
+# def wel_hel_han(message):
+#     bot.reply_to(message ,"hi \n baby")
 
-def save_member(message):
-    for member in  message.new_chat_members:
-        bot.
+# @bot.message_handler(commands=['admin'])
+# def get_admin(m):
+#     a=bot.get_chat(chat_id=m.chat.id)
+#     print(a)
+
+
+
+    
+# bot.infinity_polling()
